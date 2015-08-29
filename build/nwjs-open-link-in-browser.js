9e7bfb36f23b7eef93c8cd884de92c082d146811
sanemat <o.gata.ken@gmail.com>
2015-08-29 12:40:20 +0900

    chore(build): rebuild for browser

--- build/nwjs-open-link-in-browser.js
+++ build/nwjs-open-link-in-browser.js
@@ -2,26 +2,27 @@
 'use strict';
 
 var isNwjs = require('is-nwjs');
-var type = require('type-detect');
 
 module.exports = function (url, event) {
   if (isNwjs) {
     var gui = require('nw.gui');
-    if (type(url) === 'string') {
-      event.preventDefault();
-      gui.Shell.openExternal(url);
-    } else {
+    if (url && url.preventDefault) {
       event = url;
       event.preventDefault();
       gui.Shell.openExternal(event.target.href);
+    } else {
+      event.preventDefault();
+      gui.Shell.openExternal(url);
     }
-  } else if (type(url) === 'string') {
-    event.preventDefault();
-    window.location.href = url;
+  } else {
+    if (url && !url.preventDefault) {
+      event.preventDefault();
+      window.location.href = url;
+    }
   }
 };
 
-},{"is-nwjs":3,"nw.gui":undefined,"type-detect":5}],2:[function(require,module,exports){
+},{"is-nwjs":3,"nw.gui":undefined}],2:[function(require,module,exports){
 // shim for using process in browser
 
 var process = module.exports = {};
@@ -133,144 +134,5 @@ module.exports = isNwjs;
 module.exports = (typeof process !== 'undefined' && typeof require !== 'undefined');
 
 }).call(this,require('_process'))
-},{"_process":2}],5:[function(require,module,exports){
-module.exports = require('./lib/type');
-
-},{"./lib/type":6}],6:[function(require,module,exports){
-/*!
- * type-detect
- * Copyright(c) 2013 jake luer <jake@alogicalparadox.com>
- * MIT Licensed
- */
-
-/*!
- * Primary Exports
- */
-
-var exports = module.exports = getType;
-
-/**
- * ### typeOf (obj)
- *
- * Use several different techniques to determine
- * the type of object being tested.
- *
- *
- * @param {Mixed} object
- * @return {String} object type
- * @api public
- */
-var objectTypeRegexp = /^\[object (.*)\]$/;
-
-function getType(obj) {
-  var type = Object.prototype.toString.call(obj).match(objectTypeRegexp)[1].toLowerCase();
-  // Let "new String('')" return 'object'
-  if (typeof Promise === 'function' && obj instanceof Promise) return 'promise';
-  // PhantomJS has type "DOMWindow" for null
-  if (obj === null) return 'null';
-  // PhantomJS has type "DOMWindow" for undefined
-  if (obj === undefined) return 'undefined';
-  return type;
-}
-
-exports.Library = Library;
-
-/**
- * ### Library
- *
- * Create a repository for custom type detection.
- *
- * ```js
- * var lib = new type.Library;
- * ```
- *
- */
-
-function Library() {
-  if (!(this instanceof Library)) return new Library();
-  this.tests = {};
-}
-
-/**
- * #### .of (obj)
- *
- * Expose replacement `typeof` detection to the library.
- *
- * ```js
- * if ('string' === lib.of('hello world')) {
- *   // ...
- * }
- * ```
- *
- * @param {Mixed} object to test
- * @return {String} type
- */
-
-Library.prototype.of = getType;
-
-/**
- * #### .define (type, test)
- *
- * Add a test to for the `.test()` assertion.
- *
- * Can be defined as a regular expression:
- *
- * ```js
- * lib.define('int', /^[0-9]+$/);
- * ```
- *
- * ... or as a function:
- *
- * ```js
- * lib.define('bln', function (obj) {
- *   if ('boolean' === lib.of(obj)) return true;
- *   var blns = [ 'yes', 'no', 'true', 'false', 1, 0 ];
- *   if ('string' === lib.of(obj)) obj = obj.toLowerCase();
- *   return !! ~blns.indexOf(obj);
- * });
- * ```
- *
- * @param {String} type
- * @param {RegExp|Function} test
- * @api public
- */
-
-Library.prototype.define = function(type, test) {
-  if (arguments.length === 1) return this.tests[type];
-  this.tests[type] = test;
-  return this;
-};
-
-/**
- * #### .test (obj, test)
- *
- * Assert that an object is of type. Will first
- * check natives, and if that does not pass it will
- * use the user defined custom tests.
- *
- * ```js
- * assert(lib.test('1', 'int'));
- * assert(lib.test('yes', 'bln'));
- * ```
- *
- * @param {Mixed} object
- * @param {String} type
- * @return {Boolean} result
- * @api public
- */
-
-Library.prototype.test = function(obj, type) {
-  if (type === getType(obj)) return true;
-  var test = this.tests[type];
-
-  if (test && 'regexp' === getType(test)) {
-    return test.test(obj);
-  } else if (test && 'function' === getType(test)) {
-    return test(obj);
-  } else {
-    throw new ReferenceError('Type test "' + type + '" not defined or invalid.');
-  }
-};
-
-},{}]},{},[1])(1)
+},{"_process":2}]},{},[1])(1)
 });
